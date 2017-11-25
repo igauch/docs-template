99fcfc99d8cf27594956dd73142a6f284e17a61b
igauch <igauch@qq.com>
2017-11-25 14:24:37 +0800

    gauch

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app" class="page-component">
+    <img src="./assets/logo.png">
+    <router-view class="content"></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import elAlert from './components/main.vue'
+import demoBlock from './components/demo-block.vue'
+
+Vue.component('el-alert',elAlert);
+Vue.component('demo-block', demoBlock);
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
